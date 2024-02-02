57dffb426687c468e53b38dca9b6d8ebfa736cb3
DanGeospatial <87085567+DanGeospatial@users.noreply.github.com>
2024-02-02 14:15:16 -0500

    Update linear_reg.R
    this is a temp file don't use it for analysis

--- linear_reg.R
+++ linear_reg.R
@@ -13,6 +13,3 @@ climcor <- cor.mtest(high, conf.level = 0.95)
 #, tl.cex=0.5
 corrplot(corclim, p.mat = dNBRcor$p, type = "upper", order = "hclust", insig='blank', addCoef.col ='black', tl.col = "black", tl.srt = 45)
 
-model<-lm(high$High~high$mean_2m+high$surf_press+high$u_wind)
-summary(model)
-plot(model)
